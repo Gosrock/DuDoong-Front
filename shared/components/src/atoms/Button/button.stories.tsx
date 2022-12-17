452fa78dec203db4a332733282a78d3edd29fbd2
한규진 <david0218@naver.com>
2022-12-18 02:10:10 +0900

    test story

---
+++ shared/components/src/atoms/Button/button.stories.tsx
@@ -0,0 +1,14 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import { Button } from './';
+
+export default {
+  title: 'common/ButtonSet',
+  component: Button,
+  argTypes: {},
+} as ComponentMeta<typeof Button>;
+
+const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
+
+export const _default = Template.bind({});
+_default.args = {};
