5c71ffd035c41a174b7062b18b6b8c01f68daae5
한규진 <david0218@naver.com>
2022-12-31 01:55:21 +0900

    revert?

--- apps/ticket/next.config.js
+++ apps/ticket/next.config.js
@@ -1,12 +1,6 @@
 /* eslint-disable no-undef */
 // eslint-disable-next-line no-undef
 const withTM = require('next-transpile-modules')(['@gosrock/components']);
-const path = require('path');
 module.exports = withTM({
   // Any additional config for next goes in here
-  output: 'standalone',
-  experimental: {
-    // this includes files from the monorepo base two directories up
-    outputFileTracingRoot: path.join(__dirname, '../../'),
-  },
 });
