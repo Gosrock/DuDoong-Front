858073f1ab79718c7dc8455f2e3085c58a902145
한규진 <david0218@naver.com>
2022-12-31 14:50:02 +0900

    fix : ticket 배포 테스트용

--- apps/ticket/pages/index.tsx
+++ apps/ticket/pages/index.tsx
@@ -1,9 +1,6 @@
 import Head from 'next/head';
-import Image from 'next/image';
 import { Inter } from '@next/font/google';
 import { Button } from '@dudung/components';
-
-const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
