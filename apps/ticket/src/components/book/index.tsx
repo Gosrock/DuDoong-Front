ba8d25f8f2002e7cbaa851c51a06269b395ffe72
한규진 <david0218@naver.com>
2023-01-04 22:13:40 +0900

    feat(ticket) : 라우팅, 디렉토리 구조 세팅

---
+++ apps/ticket/src/components/book/index.tsx
@@ -0,0 +1,11 @@
+import DDHead from '@lib/utils/NextHead';
+
+const Book = () => {
+  return (
+    <>
+      <DDHead title="두둥!" />
+    </>
+  );
+};
+
+export default Book;
---
+++ apps/ticket/src/components/mypage/index.tsx
@@ -0,0 +1,12 @@
+import DDHead from '@lib/utils/NextHead';
+
+const Mypage = () => {
+  return (
+    <>
+      <DDHead title="두둥! | 마이페이지" />
+      <main>마이페이지</main>
+    </>
+  );
+};
+
+export default Mypage;
