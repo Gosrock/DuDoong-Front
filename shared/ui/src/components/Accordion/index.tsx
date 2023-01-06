90d45e87619eeb96d603e33aa7a3d953bed36d47
이한비 <99820610+AlmondBreez3@users.noreply.github.com>
2023-01-07 00:22:19 +0900

    [feature]:Accordion 구현, 스토리북 (#26)
    * feature/accordian:접었다 폈다 구현, 안에 문자열 넣기
    
    * 피드백 반영, 스토리북을 위해 만든 ListRow활용해서 한것은 삭제, 간결하게 접었다 폈다만 구현
    
    * feat(ui) : accordian 컴포넌트, 스토리
    
    * fix(ui) : accordian jsdocs
    
    Co-authored-by: 한규진 <david0218@naver.com>

---
+++ shared/ui/src/components/Accordion/index.tsx
@@ -0,0 +1,63 @@
+import { css } from '@emotion/react';
+import styled from '@emotion/styled';
+import { ReactNode, useState } from 'react';
+import { ReactComponent as Down } from '../../assets/icons/down.svg';
+import { FlexBox, ListRow, ListRowProps, PaddingSize } from '../../layout';
+export interface AccordionProps
+  extends Pick<ListRowProps, 'textTypo' | 'textColor'> {
+  title?: string;
+  padding?: PaddingSize;
+  content: ReactNode;
+}
+/**
+ * @param padding 제목 패딩 기본:[20,10]
+ * number: 상하좌우 패딩
+ * [num,num]:상하, 좌우
+ * [num,num,num,num]:상,우,하,좌
+ * @param title : 제목
+ * @param content: 내용물, 눈으로 확인해 볼 수 있게 일단 string으로 해놓았는데 기본 요소 무엇으로 해놓아야할지 정하지 x
+ * @param textTypo
+ * @param textColor
+ */
+
+export const Accordion = ({
+  title = '공연장안내',
+  padding = [16, 24],
+  content,
+  textTypo = 'Text_16',
+  textColor = 'gray_500',
+}: AccordionProps) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div>
+      <AccordianHeader onClick={() => setIsOpen((prev) => !prev)}>
+        <FlexBox align="center" justify="space-between">
+          <ListRow
+            text={title}
+            rightElement={<Handler isOpen={isOpen} />}
+            padding={padding}
+            fill
+            textTypo={textTypo}
+            textColor={textColor}
+          />
+        </FlexBox>
+      </AccordianHeader>
+      {isOpen && <div>{content}</div>}
+    </div>
+  );
+};
+
+const AccordianHeader = styled.button`
+  width: 100%;
+  border-bottom: 1px solid ${({ theme }) => theme.palette.gray_200};
+`;
+
+const Handler = styled(Down)<{ isOpen: boolean }>`
+  ${({ isOpen }) =>
+    isOpen &&
+    css`
+      transform: rotate(180deg);
+    `}
+  transition: all 0.2s ease;
+`;
