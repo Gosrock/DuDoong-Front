cfccb5e3394a7ae3f2c573ac27e80c9711fa3760
wjdtkdgns <wjdtkdgns329@naver.com>
2023-01-06 20:49:08 +0900

    feat(ui) : roundblock (#28)

--- shared/ui/src/layout/RoundBlock/index.tsx
+++ shared/ui/src/layout/RoundBlock/index.tsx
@@ -1,74 +1,34 @@
-import { FlexBox, Padding, PaddingSize } from '..';
-import { Text } from '../../components/Text';
-import { KeyOfPalette, KeyOfTypo, theme } from '../../theme';
+import { Padding, PaddingSize } from '..';
+import { KeyOfPalette } from '../../theme';
 import styled from '@emotion/styled';
 import { CSSProperties } from '@emotion/serialize';
-import { css } from '@emotion/react';
 
-// /**
-//  * @paddingSize 패딩
-//  * number : 상하좌우 패딩
-//  * [number,number] : 상하, 좌우
-//  * [number,number,number,number] : 상, 우, 하, 좌
-//  * @param text : 메인 text
-//  * @param subtext : 서브 text
-//  * @param textTypo : text 글자 크기 (기본값 : ['Text_18', 'Text_16'])
-//  * @param textColor : text 글자 크기 (기본값 : ['gray_500', 'gray_400'])
-//  * @param leftImage : 왼쪽에 들어갈 element
-//  * @param rightElement : 오른쪽에 위치시킬 element (기본값 : <></>)
-//  * --------------------
-//  * text와 subText에는 string 또는 <Text/> 컴포넌트가 들어갈 수 있습니다.
-//  * string으로 넘길때에는 textTypo와 textColor props를 함께 넘겨줍니다.
-//  * textTypo와 textColor에는 keyof@@ 또는 [keyof@@, keyof@@] 타입을 사용합니다.
-//  */
+/**
+ * @param paddingSize 패딩
+ * number : 상하좌우 패딩
+ * [number,number] : 상하, 좌우
+ * [number,number,number,number] : 상, 우, 하, 좌
+ * @param color : 배경색
+ * @param radius : border-radius 값
+ */
 
 export interface RoundBlockProps {
-  content: JSX.Element | string;
+  children: JSX.Element;
   color?: KeyOfPalette;
   paddingSize?: PaddingSize;
-  radius?: number;
-  textType?: KeyOfTypo;
-  textColor?: KeyOfPalette;
+  radius?: CSSProperties['borderRadius'];
 }
 
 export const RoundBlock = ({
-  content,
-  color = 'red_300',
-  paddingSize = [4, 12],
+  children,
+  color = 'white',
+  paddingSize = [20, 20],
   radius = 16,
-  textType = 'Text_12',
-  textColor = 'black',
 }: RoundBlockProps) => {
   return (
-    <RoundWrapper radius={radius} colorKey={color}>
-      <Padding size={paddingSize}>
-        <CustomText content={content} typo={textType} color={textColor} />
-      </Padding>
+    <RoundWrapper radius={radius} colorKey={color} size={paddingSize}>
+      {children}
     </RoundWrapper>
-  );
-};
-
-// ------------------------------------------------------
-
-const CustomText = ({
-  content,
-  typo,
-  color,
-}: {
-  content: JSX.Element | string;
-  typo: KeyOfTypo;
-  color: KeyOfPalette;
-}) => {
-  return (
-    <AlignContent>
-      {typeof content === 'string' ? (
-        <Text typo={typo} color={color}>
-          {content}
-        </Text>
-      ) : (
-        <>{content}</>
-      )}
-    </AlignContent>
   );
 };
 
@@ -79,15 +39,8 @@ interface RoundWrapperProps {
   colorKey: KeyOfPalette;
 }
 
-const RoundWrapper = styled.div<RoundWrapperProps>`
+const RoundWrapper = styled(Padding)<RoundWrapperProps>`
+  width: 100%;
   border-radius: ${({ radius }) => `${radius}px`};
-  ${({ colorKey, theme }) =>
-    colorKey &&
-    css`
-      background-color: ${theme.palette[colorKey]};
-    `};
-`;
-
-const AlignContent = styled.div`
-  text-align: center;
+  background-color: ${({ colorKey, theme }) => `${theme.palette[colorKey]}`};
 `;
--- shared/ui/src/layout/RoundBlock/roundBlock.stories.tsx
+++ shared/ui/src/layout/RoundBlock/roundBlock.stories.tsx
@@ -2,7 +2,7 @@
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import { RoundBlock } from '.';
 import { css } from '@emotion/react';
-import { Text } from '../../components';
+import { Profile, Text } from '../../components';
 import { ListRow } from '../ListRow';
 
 const ImageMock = () => {
@@ -28,31 +28,31 @@ const Template: ComponentStory<typeof RoundBlock> = (args) => (
   <RoundBlock {...args} />
 );
 
-export const OnlyText = Template.bind({});
-OnlyText.args = {
-  content: 'asd',
-  color: 'red_300',
-  paddingSize: [4, 12],
-  radius: 8,
-  textType: 'Text_12',
-  textColor: 'gray_500',
-};
-
 export const TextElement = Template.bind({});
 TextElement.args = {
-  content: <Text typo="Text_16">제목</Text>,
+  children: <Text typo="Text_16">제목</Text>,
 };
 
 export const ListRowElement = Template.bind({});
 ListRowElement.args = {
-  content: (
+  children: (
     <ListRow
-      paddingSize={[0, 0]}
+      padding={[0, 0]}
       text="text"
       subText="sub"
       rightElement={<ImageMock />}
       leftImage={<ImageMock />}
     ></ListRow>
   ),
+  paddingSize: 0,
   color: 'black',
+};
+
+export const profileElement = Template.bind({});
+profileElement.args = {
+  children: (
+    <Profile size={'big'} name={'한규진'} subText={'010 - 1234 - 5678'} />
+  ),
+  paddingSize: 10,
+  color: 'gray_200',
 };
