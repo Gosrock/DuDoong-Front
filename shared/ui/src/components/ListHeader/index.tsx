bc2046024cbb340d32257281b7c6f0c4dbbd99a4
Eugene <eugene02876@gmail.com>
2023-01-08 02:22:55 +0900

    refactor(ui) : ListHeader 피드백 리팩토링(#32)
    ListHeader 관련하여 피드백 받은 부분 코드 수정하였습니다!

--- shared/ui/src/components/ListHeader/index.tsx
+++ shared/ui/src/components/ListHeader/index.tsx
@@ -1,91 +1,145 @@
 /** @jsxImportSource @emotion/react */
-import styled from '@emotion/styled';
 import { Padding, FlexBox } from '../../layout';
 import { Text } from '../Text';
-import { TextPropsKey } from '../Text';
+import { TextType } from '../../theme';
+import { KeyOfPalette, KeyOfTypo } from '../../theme';
+import { PaddingSize } from '../../layout';
+import { ReactNode } from 'react';
 
+type TextTypo = KeyOfTypo | [KeyOfTypo, KeyOfTypo];
+type TextColor = KeyOfPalette | [KeyOfPalette, KeyOfPalette];
 
 export interface ListHeaderProps {
-    title:string,
-    description: string;
-    varient?: ListHeaderVarient;
+    title:JSX.Element | string,
+    description?: JSX.Element | string;
+    typo?: ListHeaderTypo;
+    textTypo?: TextTypo;
+    textColor?: TextColor;
+    padding?: PaddingSize;
+    rightElement?: ReactNode;
+    gap?: number;
+    tag?:boolean;
 }
 
-type ListHeaderVarient =
+type ListHeaderTypo =
   | 'listHeader_18'
   | 'listHeader_20'
   | 'listHeader_24'
   | 'listHeader_28';
 
-type ListHeaderPaddingType = {
-    [key in ListHeaderVarient]: 
-        [number,number,number,number];
-}
-
 type ListHeaderTextType = {
-    [key in ListHeaderVarient]: {
-        [key in TextPropsKey]: any;
+    [key in ListHeaderTypo]: {
+        textProp : TextType;
+        padding: PaddingSize;
     };
 };
 
 const listHeaderText: ListHeaderTextType = {
     'listHeader_18' : {
-        typo : 'Text_18_SB',
-        color : 'black',
+        textProp: {
+            typo : 'Text_18_SB',
+            color : 'black',
+            },
+        padding : [32, 24, 12, 24]
     },
     'listHeader_20' : {
-        typo : 'Header_20',
-        color : 'black',
+        textProp: {
+            typo : 'Header_20',
+            color : 'black',
+        },
+        padding : [32, 24, 16, 24]
     },
     'listHeader_24' : {
-        typo : 'Header_24',
-        color : 'black',
+        textProp: {
+            typo : 'Header_24',
+            color : 'black',
+        },
+        padding : [32, 24, 16, 24]
     },
     'listHeader_28' : {
-        typo : 'Header_28',
-        color : 'black',
-    },
+        textProp: {
+            typo : 'Header_28',
+            color : 'black',
+        },
+        padding : [32, 24, 20, 24]
+    }
 }
-
-
-const listHeaderPadding: ListHeaderPaddingType = {
-    'listHeader_18' : [32, 24, 12, 24],
-    'listHeader_20': [32, 24, 16, 24],
-    'listHeader_24': [32, 24, 16, 24],
-    'listHeader_28' : [32, 24, 20, 24],
-}
-
-
-export const TagBtnStyle = styled.button`
-  height: 26px;
-  width: 45px;
-  background: #EBECF0;
-  padding: 2px 0px;
-  align-items: right;
-`;
-
 
 export const ListHeader = ({
     description = '제목에 대한 설명이 들어가는 자리입니다. 이런식으로 두줄로 쓸 수 있습니다.',
-    varient = 'listHeader_24',
+    typo = 'listHeader_24',
     title='큰 제목',
+    textColor = [listHeaderText[typo].textProp.color, 'gray_500'], 
+    textTypo = [listHeaderText[typo].textProp.typo, 'Text_16'],
+    padding = listHeaderText[typo].padding,
+    rightElement = <></>,
+    gap = 10,
+    tag = false,
 }: ListHeaderProps) => {
     return (
-        <Wrapper>
-            <Padding size = {listHeaderPadding[varient]}>
-                <FlexBox align='left'
-                     gap={description ? '10' : '0'}
-                     justify='center'
-                     direction='column'>
-                    <Text typo = {listHeaderText[varient].typo} color = {listHeaderText[varient].color}>{title}</Text>
-                    {description ? <Text typo = 'Text_16' color = 'gray_500'>{description}</Text> : <></>}
-                </FlexBox>
+            <Padding size = {padding}>
+                <FlexBox align='left' gap={description ? gap : '0'} justify='center' direction='column'>
+                    <FlexBox id="title" align="center" justify={'space-between'}>
+                            {title && (
+                                <CustomText
+                                    text={title}
+                                    typo={getTextTypo(textTypo, 0)}
+                                    color={getTextColor(textColor, 0)}
+                                />
+                            )}
+                            {tag && rightElement }
+                        </FlexBox>
+                    {description && (
+                            <CustomText
+                                text={description}
+                                typo={getTextTypo(textTypo, 1)}
+                                color={getTextColor(textColor, 1)}
+                        />
+                        )}    
+                    </FlexBox>
             </Padding>
-        </Wrapper>
     );
 };
 
+const getTextTypo = (arg: TextTypo, index: 0 | 1): KeyOfTypo => {
+    if (arg.length === 2) {
+      return arg[index] as KeyOfTypo;
+    } else {
+      return arg as KeyOfTypo;
+    }
+  };
+  
+const getTextColor = (arg: TextColor, index: 0 | 1): KeyOfPalette => {
+    if (arg.length === 2) {
+        return arg[index] as KeyOfPalette;
+    } else {
+        return arg as KeyOfPalette;
+    }
+};
 
-const Wrapper = styled.div`
-  background: ${({ theme }) => theme.palette.gradient.linear_white};
-`;
+const isString = (text: any): text is string => {
+    return typeof text === 'string'; 
+  };
+
+const CustomText = ({
+    text,
+    typo,
+    color,
+  }: {
+    text: JSX.Element | string;
+    typo: KeyOfTypo;
+    color: KeyOfPalette;
+  }) => {
+    return (
+      <>
+        {isString(text) ? (
+          <Text typo={typo} color={color}>
+            {text}
+          </Text>
+        ) : (
+          <>{text}</>
+        )}
+      </>
+    );
+  };
+  
--- shared/ui/src/components/ListHeader/listHeader.stories.tsx
+++ shared/ui/src/components/ListHeader/listHeader.stories.tsx
@@ -1,5 +1,6 @@
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import { ListHeader } from '.';
+import { Tag } from '../Tag';
 
 export default {
     title: "listHeader",
@@ -14,18 +15,22 @@ const Template: ComponentStory<typeof ListHeader> = (args) => (
 
 export const listHeader_18 = Template.bind({});
 listHeader_18.args = {
+    rightElement: <Tag text="태그" color="mono" />
 };
 
 export const listHeader_20 = Template.bind({});
 listHeader_20.args = {
+    rightElement: <Tag text="태그" color="mono" />
 };
 
 export const listHeader_24 = Template.bind({});
 listHeader_24.args = {
+    rightElement: <Tag text="태그" color="mono" />
 };
 
 export const listHeader_28 = Template.bind({});
 listHeader_28.args = {
+    rightElement: <Tag text="태그" color="mono" />
 };
 
 
