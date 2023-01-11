087f0471af5b8f45b7324d6b7ef5dafa83781a46
한규진 <55226431+9yujin@users.noreply.github.com>
2023-01-11 20:50:18 +0900

    feat(ticket) : 카카오 로그인 (#48)
    * chore(ticket): 자잘한 에러
    
    * feat(ticket) : 카카오로그인 기능구현
    
    * feat(share) : 로딩스피너, 버튼 props 추가
    
    * feat(ticket) : 로그인 토큰 관련 로직 구현, 버튼 로딩상태
    
    * feat(ticket) : 카카오로그인 리다이렉트 오류 수정

--- apps/ticket/src/components/auth/Callback.tsx
+++ apps/ticket/src/components/auth/Callback.tsx
@@ -1,21 +1,50 @@
-import useGlobalOverlay from '@lib/hooks/useGlobalOverlay';
+import { FullScreen, SyncLoader } from '@dudoong/ui';
+import { AuthApi, OauthTokenResponse } from '@dudoong/utils';
+import { css } from '@emotion/react';
+import { useMutation } from '@tanstack/react-query';
 import { useRouter } from 'next/router';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import useAuthMutate from './useAuthMutate';
 
+//카카오 로그인 리다이렉트 페이지
 const Callback = () => {
+  const [tokens, setTokens] = useState<OauthTokenResponse>({
+    accessToken: '',
+    idToken: '',
+    refreshToken: '',
+  });
   const router = useRouter();
-  const { idToken, accessToken, refreshToken } = router.query;
-  const { openOverlay } = useGlobalOverlay();
-  console.log(idToken);
-  const openLoginTest = () => {
-    idToken && openOverlay({ content: 'register', props: { idToken } });
-  };
+  const { code } = router.query as unknown as { code: string };
+  const { oauthValidMutation } = useAuthMutate(tokens);
+
+  const oauthTokenMutation = useMutation(AuthApi.OAUTH_TOKEN, {
+    onSuccess: (data: OauthTokenResponse) => {
+      setTokens(data);
+    },
+  });
 
   useEffect(() => {
-    localStorage.setItem('accessToken', accessToken as string);
-    openLoginTest();
-  }, []);
-  return <>로그인중</>;
+    if (code) {
+      oauthTokenMutation.mutate(code);
+    }
+  }, [code]);
+  useEffect(() => {
+    if (tokens.idToken.length > 0) {
+      oauthValidMutation.mutate(tokens.idToken);
+    }
+  }, [tokens]);
+
+  return (
+    <FullScreen
+      css={css`
+        display: flex;
+        align-items: center;
+        justify-content: center;
+      `}
+    >
+      <SyncLoader />
+    </FullScreen>
+  );
 };
 
 export default Callback;
---
+++ shared/utils/src/apis/auth/AuthApi.ts
@@ -0,0 +1,56 @@
+import { axiosPublic } from '../axios';
+import { OauthInfoResponse, OauthLoginResponse } from './authReponse';
+
+export const AuthApi = {
+  REFRESH: async (refreshToken: string): Promise<OauthLoginResponse> => {
+    const response = await axiosPublic.post(
+      `/auth/token/refresh?token=${refreshToken}`,
+    );
+    return response.data.data;
+  },
+
+  OAUTH_TOKEN: async (code: string) => {
+    const response = await axiosPublic.get(`/auth/oauth/kakao?code=${code}`);
+    return response.data.data;
+  },
+
+  OAUTH_LINK: async () => {
+    const response = await axiosPublic.get('/auth/oauth/kakao/link');
+    return response.data.data;
+  },
+
+  OAUTH_VALID: async (idToken: string) => {
+    const response = await axiosPublic.get(
+      `/auth/oauth/kakao/register/valid?id_token=${idToken}`,
+    );
+    return response.data.data;
+  },
+
+  OAUTH_INFO: async (accessToken: string): Promise<OauthInfoResponse> => {
+    const response = await axiosPublic.post(
+      `/auth/oauth/kakao/info?access_token=${accessToken}`,
+    );
+    return response.data.data;
+  },
+
+  OAUTH_REGISTER: async ({
+    idToken,
+    payload,
+  }: {
+    idToken: string;
+    payload: OauthInfoResponse;
+  }): Promise<OauthLoginResponse> => {
+    const response = await axiosPublic.post(
+      `/auth/oauth/kakao/register?id_token=${idToken}`,
+      payload,
+    );
+    return response.data.data;
+  },
+
+  OAUTH_LOGIN: async (idToken: string): Promise<OauthLoginResponse> => {
+    const response = await axiosPublic.post(
+      `/auth/oauth/kakao/login?id_token=${idToken}`,
+    );
+    return response.data.data;
+  },
+};
