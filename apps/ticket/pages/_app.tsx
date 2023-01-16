d309b8affcee077bd6d9a66c4ca21cae2bfeabe2
한규진 <david0218@naver.com>
2023-01-16 15:00:01 +0900

    feat(ticket) : 초기 진입시 리프레쉬 / header 컴포넌트 수정

--- apps/ticket/pages/_app.tsx
+++ apps/ticket/pages/_app.tsx
@@ -6,29 +6,76 @@ import {
   QueryClientProvider,
 } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import type { AppProps } from 'next/app';
-import { useState } from 'react';
-import { RecoilRoot } from 'recoil';
+import type { AppProps, AppContext } from 'next/app';
+import { useMemo, useState } from 'react';
+import { MutableSnapshot, RecoilRoot } from 'recoil';
 import 'react-spring-bottom-sheet/dist/style.css';
 import GlobalOverlay from '@components/shared/overlay/GlobalOverlay';
+import { AuthApi } from '@dudoong/utils';
+import cookies from 'next-cookies';
+import { authState, AuthStateType } from '@store/auth';
+import useGlobalOverlay from '@lib/hooks/useGlobalOverlay';
+import MainLayout from '@components/shared/Main';
 
-export default function App({ Component, pageProps }: AppProps) {
+interface MyAppProps extends AppProps {
+  auth: AuthStateType | null;
+}
+
+function MyApp({ Component, pageProps, auth }: MyAppProps) {
+  const initializer = useMemo(
+    () =>
+      ({ set }: MutableSnapshot) => {
+        if (auth) {
+          set(authState, auth);
+        }
+      },
+    [auth],
+  );
   const [queryClient] = useState(() => new QueryClient());
 
   return (
     <ThemeProvider theme={theme}>
-      <RecoilRoot>
+      <RecoilRoot initializeState={initializer}>
         <QueryClientProvider client={queryClient}>
           <ReactQueryDevtools initialIsOpen={false} />
           <Hydrate state={pageProps.dehydratedState}>
             <Global styles={globalStyle} />
-            <Layout>
+            <MainLayout>
               <Component {...pageProps} />
               <GlobalOverlay />
-            </Layout>
+            </MainLayout>
           </Hydrate>
         </QueryClientProvider>
       </RecoilRoot>
     </ThemeProvider>
   );
 }
+
+MyApp.getInitialProps = async (context: AppContext) => {
+  const { ctx, Component } = context;
+  const refreshToken = cookies(ctx).refreshToken;
+  //console.log(refreshToken);
+  let pageProps = {};
+  let auth: AuthStateType | null;
+
+  try {
+    const response = await AuthApi.REFRESH(refreshToken!);
+    auth = {
+      userProfile: response.userProfile,
+      accessToken: response.accessToken,
+      isAuthenticated: true,
+      callbackUrl: '/',
+    };
+  } catch (err) {
+    auth = null;
+  }
+
+  if (Component.getInitialProps) {
+    // Component의 context로 ctx를 넣어주자
+    pageProps = await Component.getInitialProps(ctx);
+  }
+  // return한 값은 해당 컴포넌트의 props로 들어가게 됩니다.
+  return { pageProps, auth };
+};
+
+export default MyApp;
