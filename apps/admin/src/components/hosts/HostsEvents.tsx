26dc218b062ca77bb9d41fd9f69f1b5112cf7267
Hanbee Lee <bee1162@naver.com>
2023-02-18 03:23:59 +0900

    fix:코드정리 #144

--- apps/admin/src/components/hosts/HostsEvents.tsx
+++ apps/admin/src/components/hosts/HostsEvents.tsx
@@ -1,5 +1,4 @@
 import EventItem from '@components/home/EventItem';
-import { PaddingSize } from '@dudoong/ui';
 import BorderBox from '@dudoong/ui/src/layout/BorderBox';
 import { useInfiniteQueries } from '@dudoong/utils';
 import { css } from '@emotion/react';
@@ -10,16 +9,11 @@ import NoEventPage from './NoEventPage';
 
 const HostsEvents = () => {
   const hostId = useLocation().pathname.split('/')[2];
-  const ADMIN_EVENTS_MAP = {
-    padding: [16, 16],
-    apiFunction: () => HostApi.GET_HOST_EVENTS(hostId),
-    item: EventItem,
-  };
   const { infiniteListElement, isEmpty } =
     useInfiniteQueries<EventProfileResponse>(
       'Hostevent',
-      ADMIN_EVENTS_MAP.apiFunction,
-      ADMIN_EVENTS_MAP.item,
+      () => HostApi.GET_HOST_EVENTS(hostId),
+      EventItem,
     );
 
   if (isEmpty) {
@@ -32,7 +26,7 @@ const HostsEvents = () => {
     return (
       <>
         <BorderBox
-          padding={ADMIN_EVENTS_MAP.padding as PaddingSize}
+          padding={[16, 16]}
           css={css`
             & > div > .host-divider:nth-last-of-type(2) {
               display: none;
