f00b3049dd72db029506653fc21396e222792f7a
강나연 <seoultownroad@naver.com>
2023-02-18 18:24:08 +0900

    feat: 새 티켓 만들기 페이지 페이드인 효과

--- apps/admin/src/components/events/tickets/newtickets/form/TicketForm.tsx
+++ apps/admin/src/components/events/tickets/newtickets/form/TicketForm.tsx
@@ -1,5 +1,4 @@
 import { payType } from '@lib/apis/ticket/ticketType';
-import React from 'react';
 import {
   FieldErrors,
   FieldValues,
@@ -10,6 +9,8 @@ import {
 import DudoongTicketForm from './DudoongTicketForm';
 import FreeTicketForm from './FreeTicketForm';
 import PaidTicketForm from './PaidTicketForm';
+import { keyframes } from '@emotion/react';
+import styled from '@emotion/styled';
 
 export interface TicketFormProps {
   select: payType;
@@ -20,14 +21,68 @@ export interface TicketFormProps {
 }
 
 const TicketForm = ({ select, ...props }: TicketFormProps) => {
-  if (select === '두둥티켓') {
-    return <DudoongTicketForm {...props} />;
-  } else if (select === '무료티켓') {
-    return <FreeTicketForm />;
-  } else {
-    // select === '유료티켓'
-    return <PaidTicketForm />;
-  }
+  return (
+    <Wrapper>
+      {select === '두둥티켓' ? (
+        <div className="dudoong-ticket">
+          <DudoongTicketForm {...props} />
+        </div>
+      ) : select === '무료티켓' ? (
+        <div className="free-ticket">
+          <FreeTicketForm />
+        </div>
+      ) : select === '유료티켓' ? (
+        <div className="paid-ticket">
+          <PaidTicketForm />
+        </div>
+      ) : (
+        <></>
+      )}
+    </Wrapper>
+  );
 };
 
 export default TicketForm;
+
+const Wrapper = styled.div`
+  // 각각 줘야만 먹는데 다른 방법이 없을까~...
+
+  @keyframes fadeIn1 {
+    from {
+      opacity: 0;
+    }
+    to {
+      opacity: 1;
+    }
+  }
+
+  @keyframes fadeIn2 {
+    from {
+      opacity: 0;
+    }
+    to {
+      opacity: 1;
+    }
+  }
+
+  @keyframes fadeIn3 {
+    from {
+      opacity: 0;
+    }
+    to {
+      opacity: 1;
+    }
+  }
+
+  .dudoong-ticket {
+    animation: 1s forwards fadeIn1 ease-out;
+  }
+
+  .free-ticket {
+    animation: 1s forwards fadeIn2 ease-out;
+  }
+
+  .paid-ticket {
+    animation: 1s forwards fadeIn3 ease-out;
+  }
+`;
