d6ada842cec28b7b34d969605df4cf5fa3e463b5
강나연 <seoultownroad@naver.com>
2023-02-24 17:38:04 +0900

    feat(ticket): OrderItem 컴포넌트 추가

---
+++ apps/ticket/src/components/mypage/OrderItem.tsx
@@ -0,0 +1,95 @@
+import {
+  FlexBox,
+  ListHeader,
+  ListRow,
+  Spacing,
+  Tag,
+  TagColorKey,
+  Text,
+  theme,
+} from '@dudoong/ui';
+import styled from '@emotion/styled';
+import { OrderListResponse, StageType } from '@lib/apis/order/orderType';
+
+import Image from 'next/image';
+
+type TagColorType = {
+  [key in StageType]: TagColorKey;
+};
+
+const TAG_COLOR: TagColorType = {
+  승인대기: 'red',
+  입장완료: 'main',
+  관람예정: 'main',
+  입장중: 'main',
+  취소됨: 'mono',
+};
+
+const OrderItem = (prop: OrderListResponse) => {
+  return (
+    <Wrapper>
+      <Image
+        src={prop.eventProfile.posterImage}
+        alt=""
+        className="poster"
+        width={120}
+        height={166}
+      />
+      <ContentWrapper>
+        <FlexBox align="flex-start" direction="column" gap={5}>
+          <Tag text={prop.stage} color={TAG_COLOR[prop.stage]} size={'sm'} />
+          <Text className="title" typo="P_Button_16_SB">
+            {prop.eventProfile.name}
+          </Text>
+        </FlexBox>
+        <ListRow
+          text={prop.eventProfile.startAt}
+          textTypo={'P_Text_12_M'}
+          subText={`${prop.itemName} ${prop.totalQuantity}매 | 주문번호 ${prop.orderNo}`}
+          padding={0}
+        />
+      </ContentWrapper>
+    </Wrapper>
+  );
+};
+
+export default OrderItem;
+
+const Wrapper = styled.div`
+  background-color: ${theme.palette.white};
+  border-radius: 12px;
+  border: 1px solid ${theme.palette.black};
+
+  width: 100%;
+  height: 166px;
+
+  overflow: hidden;
+
+  display: flex;
+  justify-content: space-between;
+
+  .poster {
+    width: auto;
+    height: 100%;
+
+    object-fit: cover;
+  }
+`;
+
+const ContentWrapper = styled.div`
+  padding: 15px 15px 10px 15px;
+  width: 100%;
+
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: space-between;
+  gap: 5px;
+
+  .title {
+    word-break: break-all;
+    word-wrap: break-word;
+    white-space: -moz-pre-wrap;
+    white-space: pre-wrap;
+  }
+`;
