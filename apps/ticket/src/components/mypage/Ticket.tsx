522ef386a5b4d375684b4c332618111496dd232c
한규진 <david0218@naver.com>
2023-02-24 13:52:28 +0900

    chore : 좀 건들다가 api 수정 대기중..

--- apps/ticket/src/components/mypage/Ticket.tsx
+++ apps/ticket/src/components/mypage/Ticket.tsx
@@ -1,10 +1,20 @@
+import Main from '@components/shared/Layout/Main';
 import DDHead from '@components/shared/Layout/NextHead';
+import { NavBar } from '@dudoong/ui';
+import { useRouter } from 'next/router';
 
 const Ticket = () => {
+  const {
+    query: { id },
+    back,
+  } = useRouter();
+  console.log(id);
   return (
     <>
       <DDHead title="두둥!" />
-      <main>모바일티켓</main>
+      <Main>
+        <NavBar label="모바일 티켓" backHandler={back} />
+      </Main>
     </>
   );
 };
