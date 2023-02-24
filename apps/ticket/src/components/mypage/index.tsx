951b98f10a2dabb1c555788e02bd4e19cf8433d1
강나연 <seoultownroad@naver.com>
2023-02-24 19:53:17 +0900

    feat(ticket): Shortcuts 컴포넌트 추가

--- apps/ticket/src/components/mypage/index.tsx
+++ apps/ticket/src/components/mypage/index.tsx
@@ -1,17 +1,58 @@
 import { Divider, ListHeader, Padding, Profile } from '@dudoong/ui';
 import DDHead from '@components/shared/Layout/NextHead';
+import { useInfiniteQueries } from '@dudoong/utils';
+
+import { authState } from '@store/auth';
+import { useRecoilValue } from 'recoil';
+
+import OrderItem from './OrderItem';
+import styled from '@emotion/styled';
+import { OrderListResponse } from '@lib/apis/order/orderType';
+import { OrderApi } from '@lib/apis/order/OrderApi';
+import Shortcuts from '@components/shared/Shortcuts';
+import Main from '@components/shared/Layout/Main';
 
 const Mypage = () => {
+  const { userProfile } = useRecoilValue(authState);
+
+  const { infiniteListElement, isEmpty } =
+    useInfiniteQueries<OrderListResponse>(
+      ['mypage'],
+      ({ pageParam = 0 }) =>
+        OrderApi.GET_ORDERS({
+          pageParam,
+        }),
+      OrderItem,
+    );
+
   return (
-    <>
+    <Main>
       <DDHead title="두둥! | 마이페이지" />
       <ListHeader title={'마이페이지'} size={'listHeader_28'} />
       <Padding size={[20, 24]}>
-        <Profile size="big" name="한규진" subText="010-5536-4937" />
+        <Profile
+          size="big"
+          image={userProfile?.profileImage}
+          name={userProfile?.name || ''}
+          subText={'010-7554-6670'}
+        />
       </Padding>
       <Divider />
+      <Padding size={[10, 24, 10, 24]}>{infiniteListElement}</Padding>
       <ListHeader title={'바로가기'} size={'listHeader_20'} />
-    </>
+
+      <Shortcuts text="내 예매내역" url="/history" />
+      <Shortcuts text="내 쿠폰함" url="/" />
+      <Divider />
+      <Shortcuts text="공연 시작하기" url="/" />
+      <Divider />
+      <Shortcuts text="고스락 깃허브" url="/" />
+      <Shortcuts text="개발자 이야기" url="/" />
+      <Divider />
+      <Shortcuts text="로그아웃" url="/" />
+      <Divider />
+      <Shortcuts text="회원탈퇴" textColor="red_300" url="/" />
+    </Main>
   );
 };
 
