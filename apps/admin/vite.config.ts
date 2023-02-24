0791a82e67a14872eaa3a5e3338f3a7763c871f2
Hanbee Lee <bee1162@naver.com>
2023-02-24 23:57:34 +0900

    fix:env에 카카오키 환경변수 설정하기 #193

--- apps/admin/vite.config.ts
+++ apps/admin/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
+import VitePluginHtmlEnv from 'vite-plugin-html-env';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,6 +13,8 @@ export default defineConfig({
         plugins: ['@emotion/babel-plugin'],
       },
     }),
+    VitePluginHtmlEnv(),
+    VitePluginHtmlEnv({ compiler: true }),
   ],
   resolve: {
     alias: [
