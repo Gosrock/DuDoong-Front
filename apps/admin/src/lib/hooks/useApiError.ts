080bc8eca8465b829c301b20fec3e1edee055f6c
wjdtkdgns <wjdtkdgns329@naver.com>
2023-02-27 02:25:00 +0900

    fix(admin) : universal-cookie로 변경, 과거 로직 롤백

--- apps/admin/src/lib/hooks/useApiError.ts
+++ apps/admin/src/lib/hooks/useApiError.ts
@@ -3,8 +3,8 @@ import { useCallback } from 'react';
 import { AxiosError } from 'axios';
 import { useNavigate } from 'react-router-dom';
 import useRefresh from './auth/useRefresh';
-import { getCookie } from '@lib/utils/cookie';
-import { useCookies } from 'react-cookie';
+import { getCookie, removeCookie } from '@lib/utils/cookie';
+// import { useCookies } from 'react-cookie';
 import { useQueryClient } from '@tanstack/react-query';
 
 export interface TCustomErrorResponse {
@@ -20,11 +20,12 @@ const useApiError = () => {
   const { setToast } = useToastify();
   const navigate = useNavigate();
   const { refreshMutate } = useRefresh();
-  const [cookies, ,] = useCookies();
+  // const [cookies, ,] = useCookies();
   const queryClient = useQueryClient();
 
   const authenticationErrorHandler = () => {
-    const refreshToken = cookies.refreshToken;
+    // const refreshToken = cookies.refreshToken;
+    const refreshToken = getCookie('refreshToken');
     if (refreshToken) {
       refreshMutate(refreshToken, {
         onSuccess: () => {
@@ -32,8 +33,10 @@ const useApiError = () => {
           navigate('/');
         },
         onError: () => {
-          setToast({ comment: '다시 로그인 해주세요!' });
-          navigate('/login');
+          // setToast({ comment: '다시 로그인 해주세요!' });
+          // removeCookie('refreshToken');
+          // removeCookie('accessToken');
+          // navigate('/login');
         },
       });
     } else {
@@ -43,7 +46,35 @@ const useApiError = () => {
 
   const handleError = useCallback((axiosError: AxiosError) => {
     const errorResponse = axiosError.response?.data as TCustomErrorResponse;
-    const { status, reason } = errorResponse;
+    const { status, reason, code } = errorResponse;
+    // if (code === 'AUTH_403_1') {
+    //   console.log();
+    //   removeCookie('refreshToken');
+    //   removeCookie('accessToken');
+    //   console.log(
+    //     '세팅확인',
+    //     getCookie('accessToken'),
+    //     getCookie('refreshToken'),
+    //   );
+    //   setToast({ comment: '다시 로그인 해주세요!' });
+    // } else {
+    //   switch (status) {
+    //     // BadRequestException | ValidationError
+    //     case 400:
+    //     case 404:
+    //     case 500:
+    //       setToast({ comment: reason });
+    //       break;
+    //     // authentication error
+    //     case 401:
+    //     case 403:
+    //       authenticationErrorHandler();
+    //       break;
+    //     default:
+    //       setToast({ comment: reason });
+    //       break;
+    //   }
+    // }
     switch (status) {
       // BadRequestException | ValidationError
       case 400:
@@ -54,8 +85,7 @@ const useApiError = () => {
       // authentication error
       case 401:
       case 403:
-        console.log(status);
-        authenticationErrorHandler();
+        navigate('/login');
         break;
       default:
         setToast({ comment: reason });
