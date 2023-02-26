914a6ca654080bc8ce4ed8d5d3004b9ec28597f0
한규진 <david0218@naver.com>
2023-02-26 19:58:16 +0900

    fix : 개발환경일때만 쿠키 직접 세팅

--- apps/admin/src/components/shared/auth/RefuseAuth.tsx
+++ apps/admin/src/components/shared/auth/RefuseAuth.tsx
@@ -3,12 +3,12 @@ import { useRecoilValue } from 'recoil';
 import { authState } from '@store/auth';
 
 const RefuseAuth = () => {
-  /* const auth = useRecoilValue(authState);
+  const auth = useRecoilValue(authState);
   // 엑세스 있으면 홈으로
   if (auth.isAuthenticated) {
     return <Navigate replace to="/" />;
   }
-  // 둘 다 없으면 로그인 */
+  // 둘 다 없으면 로그인
   return <Outlet />;
 };
 
