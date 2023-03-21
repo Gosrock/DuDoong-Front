3d697fffc56312d02fbd4671d33eb0a214a35c0d
한규진 <david0218@naver.com>
2023-03-21 17:02:15 +0900

    fix : 빌드 오류 수정

--- apps/ticket/src/components/mypage/Ticket/QrBlank.tsx
+++ apps/ticket/src/components/mypage/Ticket/QrBlank.tsx
@@ -1,28 +1,8 @@
 import { NavBar, Spacing } from '@dudoong/ui';
 import { TicketApi } from '@lib/apis/ticket/TicketApi';
 import { useQuery, useQueryClient } from '@tanstack/react-query';
-import dynamic from 'next/dynamic';
 import { useRouter } from 'next/router';
-import QRCodeStyling from 'qr-code-styling';
-import { useEffect, useRef } from 'react';
 import QrSheetContainer from './QrSheetContainer';
-
-const qrCode = new QRCodeStyling({
-  width: 250,
-  height: 250,
-  margin: 0,
-  dotsOptions: {
-    color: 'black',
-    type: 'rounded',
-  },
-  cornersSquareOptions: {
-    type: 'extra-rounded',
-  },
-  backgroundOptions: { color: '#ffffff' },
-  imageOptions: {
-    crossOrigin: 'anonymous',
-  },
-});
 
 const QrBlank = () => {
   const { query, back } = useRouter();
