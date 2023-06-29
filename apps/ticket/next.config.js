afccd11a4e8d6c8737534f3b3ade4f9a361d6d8f
한규진 <david0218@naver.com>
2023-06-29 11:38:11 +0900

    refactor : 랜딩 섹션 레이지로딩

--- apps/ticket/next.config.js
+++ apps/ticket/next.config.js
@@ -2,42 +2,48 @@
 // eslint-disable-next-line no-undef
 const withInterceptStdout = require('next-intercept-stdout');
 
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
 const withTM = require('next-transpile-modules')([
   '@dudoong/ui',
   '@dudoong/utils',
   'date-fns',
 ]);
 
-module.exports = withInterceptStdout(
-  withTM({
-    // Any additional config for next goes in here
-    swcMinify: true,
-    webpack: (config, options) => {
-      config.module.rules.push({
-        test: /\.svg$/,
-        use: [
-          options.defaultLoaders.babel,
-          {
-            loader: '@svgr/webpack',
-            options: { babel: false },
-          },
-        ],
-      });
-      return config;
-    },
-    images: {
-      domains: ['asset.dudoong.com'],
-    },
-    rewrites: async () => [
-      {
-        source: '/meta/term',
-        destination: '/term.html',
+module.exports = withBundleAnalyzer(
+  withInterceptStdout(
+    withTM({
+      // Any additional config for next goes in here
+      swcMinify: true,
+      webpack: (config, options) => {
+        config.module.rules.push({
+          test: /\.svg$/,
+          use: [
+            options.defaultLoaders.babel,
+            {
+              loader: '@svgr/webpack',
+              options: { babel: false },
+            },
+          ],
+        });
+        return config;
       },
-      {
-        source: '/meta/privacy',
-        destination: '/privacy.html',
+      images: {
+        domains: ['asset.dudoong.com'],
       },
-    ],
-  }),
-  (text) => (text.includes('Duplicate atom key') ? '' : text),
+      rewrites: async () => [
+        {
+          source: '/meta/term',
+          destination: '/term.html',
+        },
+        {
+          source: '/meta/privacy',
+          destination: '/privacy.html',
+        },
+      ],
+    }),
+    (text) => (text.includes('Duplicate atom key') ? '' : text),
+  ),
 );
--- apps/ticket/src/components/home/Landing/index.tsx
+++ apps/ticket/src/components/home/Landing/index.tsx
@@ -1,12 +1,18 @@
 import { Footer } from '@dudoong/ui';
 import DDHead from '@components/shared/Layout/NextHead';
 import Title from './Title';
-import IntroSection from './IntroSection';
-import FeatureSection from './FeatureSection';
-import TicketSection from './TicketSection';
-import OutroSection from './Outro';
+//import IntroSection from './IntroSection';
+//import FeatureSection from './FeatureSection';
+//import TicketSection from './TicketSection';
+//import OutroSection from './Outro';
 import { css } from '@emotion/react';
+import React from 'react';
 const Landing = () => {
+  const IntroSection = React.lazy(() => import('./IntroSection'));
+  const FeatureSection = React.lazy(() => import('./FeatureSection'));
+  const TicketSection = React.lazy(() => import('./TicketSection'));
+  const OutroSection = React.lazy(() => import('./Outro'));
+
   return (
     <>
       <DDHead
