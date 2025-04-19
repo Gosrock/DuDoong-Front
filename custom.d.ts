490b5ac94dfd6e854b6a1aaa51bc9c440e4415c5
한규진 <55226431+9yujin@users.noreply.github.com>
2025-04-19 22:10:28 +0900

    feat : 카카오애드핏 (#325)
    Co-authored-by: 9yujin <9yujin@toss.im>

--- custom.d.ts
+++ custom.d.ts
@@ -2,3 +2,13 @@ declare module '*.svg' {
   const value: React.FunctionComponent<React.SVGAttributes<SVGElement>>;
   export default value;
 }
+
+declare global {
+  interface Window {
+    adfit?: {
+      destroy: (unit: string) => void;
+    };
+  }
+}
+
+export {};
